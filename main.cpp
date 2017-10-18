dac2cf7eb953c159a6e0ce74f1a3122f2408e245
Maneadragos <32910098+Maneadragos@users.noreply.github.com>
2017-10-18 21:34:36 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int main()
 {
-    int a,b;
-        cout<<"dati valori pentru a,b:";
-        cin>>a>>b;
-        cout<<"suma:"<<a+b<<'\n';
-        cout<<"diferenta:";
-    if(a>b)
-        cout<<a-b<<'\n';
-    else
-        cout<<b-a<<'\n';
-        cout<<"produsul:"<<a*b<<'\n';
-        cout<<"impartirea:";
-    if(a>b)
-        cout<<a/b<<'\n';
-    else
-        cout<<b/a<<'\n';
-        cout<<"impartirea cu mod:";
-    if(a>b)
-        cout<< a%b<<'\n';
-    else
-        cout<< b%a<<'\n';
-
+    int n1,n2,b=0;
+        cout<<"dati valori variabilelor:";
+        cin>>n1>>n2;
+        cout<<"radacina patrata a lui n1:"<<n1*n1<<'\n';
+        cout<<"radacina patrata a lui n2:"<<n2*n2<<'\n';
+        cout<<"n1 la puterea n2:"<<pow(n1,n2)<<'\n';
+        cout<<"inversul lui n1:";
+    while (n1!=0)
+        {
+         b=b*10+n1%10;
+         n1=n1/10;
+        }
+        cout<<b<<'\n';
+        b=0;
+        cout<<"inversul lui n2:";
+    while (n2!=0)
+      {
+        b=b*10+n2%10;
+        n2=n2/10;
+      }
+        cout<<b<<'\n';
     return 0;
 }
